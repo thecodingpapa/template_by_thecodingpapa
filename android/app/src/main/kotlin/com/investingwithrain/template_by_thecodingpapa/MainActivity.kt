1c28597226eb9fe9d83e7cb62afb2908033ed4c8
Victorer <victor@myicellar.com>
2021-02-04 16:51:53 +0900

    -initial template

---
+++ android/app/src/main/kotlin/com/investingwithrain/template_by_thecodingpapa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.investingwithrain.template_by_thecodingpapa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends Page {
+  static final pageName = "AuthPage";
+
+  AuthPage({Key key}) : super(key: key);
+
+  Route createRoute(BuildContext context) {
+    return MaterialPageRoute(
+      settings: this,
+      builder: (BuildContext context) {
+        return AuthWidget();
+      },
+    );
+  }
+}
+
+class AuthWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AuthWidgetState();
+}
+
+class _AuthWidgetState extends State<AuthWidget> {
+  bool _value = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+    return Material(
+      child: Container(
+          color: Colors.redAccent,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                  height: height * 0.7,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          colorFilter: ColorFilter.mode(
+                              Colors.black.withOpacity(.5), BlendMode.darken),
+                          image: AssetImage('assets/image.jpg'),
+                          fit: BoxFit.cover))),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: FlutterLogo(size: 100),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Stack(
+                      alignment: AlignmentDirectional.topStart,
+                      children: <Widget>[
+                        Padding(
+                            padding: EdgeInsets.only(top: 70),
+                            child: Text('LOGIN',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600))),
+                        Padding(
+                          padding: EdgeInsets.only(top: 110),
+                          child: Container(
+                              height: height * 0.25,
+                              color: Colors.transparent,
+                              child: Column(
+                                children: <Widget>[
+                                  TextField(
+                                      cursorColor: Colors.white,
+                                      decoration: InputDecoration(
+                                        filled: true,
+                                        fillColor:
+                                            Colors.grey[700].withOpacity(0.5),
+                                        hintText: 'Username',
+                                        hintStyle: TextStyle(
+                                            color: Colors.grey[400],
+                                            fontSize: 14),
+                                        focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            borderSide: BorderSide(
+                                                color: Colors.white, width: 2)),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            borderSide: BorderSide(
+                                                color: Colors.grey, width: 2)),
+                                      )),
+                                  SizedBox(height: 20),
+                                  TextField(
+                                      obscureText: true,
+                                      cursorColor: Colors.white,
+                                      decoration: InputDecoration(
+                                        filled: true,
+                                        fillColor:
+                                            Colors.grey[700].withOpacity(0.5),
+                                        hintText: 'Password',
+                                        hintStyle: TextStyle(
+                                            color: Colors.grey[400],
+                                            fontSize: 14),
+                                        focusedBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            borderSide: BorderSide(
+                                                color: Colors.white, width: 2)),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            borderSide: BorderSide(
+                                                color: Colors.grey, width: 2)),
+                                      ))
+                                ],
+                              )),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 300),
+                          child: Container(
+                            height: height * 0.08,
+                            width: width,
+                            child: RaisedButton(
+                              color: Colors.white,
+                              onPressed: () {},
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Text(
+                                'LOGIN',
+                              ),
+                              textColor: Colors.redAccent,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 380),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(children: [
+                                Checkbox(
+                                    activeColor: Colors.white,
+                                    checkColor: Colors.black,
+                                    value: _value,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _value = value;
+                                      });
+                                    }),
+                                Text('Keep me logged in',
+                                    style: TextStyle(color: Colors.white)),
+                              ]),
+                              InkWell(
+                                onTap: () {},
+                                child: Text('Forgot password?',
+                                    style: TextStyle(color: Colors.white)),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 440),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Don't have an account?",
+                                  style: TextStyle(color: Colors.white)),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: Text("SIGN UP",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold)),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
